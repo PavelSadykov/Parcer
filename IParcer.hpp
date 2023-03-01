052d9b41537e57cfab4660796077253af07e17ae
Pavel Sadykov <2439747@gmail.com>
2023-03-02 01:19:00 +0500

    Create IParcer.hpp

---
+++ IParcer.hpp
@@ -0,0 +1,21 @@
+//
+//  IParcer.hpp
+//  ParcerINIFile
+//
+//  Created by Павел on 28.02.2023.
+//
+
+#ifndef IParcer_hpp
+#define IParcer_hpp
+
+#include <stdio.h>
+
+ #include <iostream>
+ #include <map>
+ class IParcer{
+ public:
+     IParcer() = default;
+     virtual ~IParcer() = default;
+ };
+
+#endif /* IParcer_hpp */
